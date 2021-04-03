3a6f32dcfdc57f2926962fad68b516233a56d93a
Wendy Fgigueroa <camomila444@gmail>
2021-04-03 17:00:28 -0400

    working on

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import data from '../data';
+
+class About extends Component {
+
+  state = { }
+  render() {
+
+        return (
+            <div className='about'>
+            <div className='about-cpntent'>
+            <h1><Fade bottom cascade>About.</Fade></h1> 
+            <Fade bottom> 
+                 <p>{data.abouttext}</p>
+                </Fade> 
+
+                </div> 
+              {data.showAboutImage ? <img src={data.about.Image} alt='about image'></img> : null }
+                
+            </div>
+        );
+
+  }
+
+}
+
+export default About;
