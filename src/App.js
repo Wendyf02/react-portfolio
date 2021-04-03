93960cbd0c9c33bfedb7ef9d6f674306f861db38
Wendy Fgigueroa <camomila444@gmail>
2021-04-02 21:24:08 -0400

    working on

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar'
+import Header from './components/Header';
+import About from  './components/About';
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+
+      <div className="App-header">
+        <Header></Header>
+      </div>
+
+      <div classNmae="About">
+        <About></About>
+      </div>
+
+      <div className="Portfolio">
+        <Portfolio></Portfolio>
+      </div>
+      <div className="Contact">
+       <Contact></Contact>
+      </div>
+      <div className="Footer">
+       <Footer></Footer>
+      </div>
+     
     </div>
   );
 }
